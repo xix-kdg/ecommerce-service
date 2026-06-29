ba0f194073803e99c988b1199dad60f2b9da39b6
KG <kevindeveza.guido@gmail.com>
2026-06-30 07:32:08 +0800

    [1] Refactor to avoid hardcoded values

--- src/main/java/com/kg/ecommerce/constant/ApiRoutes.java
+++ src/main/java/com/kg/ecommerce/constant/ApiRoutes.java
@@ -7,6 +7,8 @@ public final class ApiRoutes {
     public static final class Products {
         public static final String BASE = "/api";
         public static final String PRODUCTS = "/products";
+        public static final String PRODUCTS_ID = "/products/{id}";
+        public static final String PRODUCT = "/product";
         public static final String SEARCH = "/products/search";
     }
 }
